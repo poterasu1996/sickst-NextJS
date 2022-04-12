4adee1fc0cd3b268874d77df5356a3636bc008c8
Ionut <i.poterasu@yahoo.com>
2022-04-13 00:54:54 +0300

    server setup

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const connectDB = require("./config/db");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+// connect to db
+connectDB();
+
+const app = express();
+
+// Logging - only for dev
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+
+// Handlebars - optional if we work with React
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
