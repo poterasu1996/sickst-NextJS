5cbace9abbe03a64645eab6e7cf59639565cb33a
Poterasu <i.poterasu@yahoo.com>
2022-04-14 10:20:41 +0300

    google authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,14 +4,16 @@ const router = express.Router();
 
 // @desc    Login/Landing page
 // @route   GET/
-router.get('/', (req, res) => {
-    res.send('Login');
+router.get("/", (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
 });
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get('/dashboard', (req, res) => {
-    res.send('Dashboard');
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 module.exports = router;
