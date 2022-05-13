042c24088c89086d31007289bc193860582dc8f0
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2022-05-13 14:29:52 +0300

    Proxy config

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://sickst.ro/cms/'),
   app: {
-    proxy: env.bool("IS_PROXY"),
     keys: env.array('APP_KEYS'),
   },
+  proxy: env.bool("IS_PROXY"),
+  url: env('PUBLIC_URL', 'https://sickst.ro/cms/'),
 });
