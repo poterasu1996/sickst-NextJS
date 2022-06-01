91c715650a80f9205e3641e04d4ebcf0dfc99ff2
Ionut <i.poterasu@yahoo.com>
2022-06-01 09:40:03 +0300

    added css for google button

--- client/pages/account/login.js
+++ client/pages/account/login.js
@@ -1,59 +1,51 @@
-import Link from 'next/link';
-import LogInForm from '../../components/auth/LoginForm';
-import GoogleLogin from 'react-google-login';
+import Link from "next/link";
+import LogInForm from "../../components/auth/LoginForm";
+import gIcon from "../../public/img/svg/g-icon.svg";
 
 const Login = () => {
 
-    // const onSuccess = (res) => {
-    //     console.log("SUCCESS", res.profileObj);
-    // }
-
-    // const onFailure = (res) => {
-    //     console.log('FAILED', res);
-    // }
-
-    return (
-        <div className="main-content">
-            <div className="main-body">
-                <div className="container register-page">
-                    <div className="row">
-                        <div className="col-lg-5 title">
-                            <span>Log in to your account</span>
-                        </div>
-                        <div className="col-lg-5">
-                            <div className="form">
-                                <LogInForm />
-                            </div>
-                        </div>
-                        <div className="col-lg-5 login">
-                            <div className="login-link w-100 justify-content-between">
-                                <a href="#">Forgot your password?</a>
-                                <Link href='register'>Don't have an account?</Link>
-                            </div>
-                            <div className="split">
-                                <span className="line"></span>
-                                <span className="text">or</span>
-                                <span className="line"></span>
-                            </div>{console.log(process.env.NEXT_PUBLIC_GA_ID)}
-                            {/* <GoogleLogin 
-                                clientId={process.env.NEXT_PUBLIC_GA_ID}
-                                buttonText='Log in with Google'
-                                onSuccess={onSuccess}
-                                onFailure={onFailure}
-                                cookiePolicy={'single_host_origin'}
-                                isSignedIn={true}
-                                className="g-auth"
-                            /> */}
-                            <div className='google-login'>
-                                <button onClick={() => 
-                                (window.location = `${process.env.NEXT_PUBLIC_STRAPI_ROOTURL}/api/connect/google`)}>Login via google</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="main-content">
+      <div className="main-body">
+        <div className="container register-page">
+          <div className="row">
+            <div className="col-lg-5 title">
+              <span>Log in to your account</span>
             </div>
+            <div className="col-lg-5">
+              <div className="form">
+                <LogInForm />
+              </div>
+            </div>
+            <div className="col-lg-5 login">
+              <div className="login-link w-100 justify-content-between">
+                <a href="#">Forgot your password?</a>
+                <Link href="register">Don't have an account?</Link>
+              </div>
+              <div className="split">
+                <span className="line"></span>
+                <span className="text">or</span>
+                <span className="line"></span>
+              </div>
+              {console.log(process.env.NEXT_PUBLIC_GA_ID)}
+             
+              <button
+                className="g-auth"
+                onClick={() =>
+                  (window.location = `${process.env.NEXT_PUBLIC_STRAPI_ROOTURL}/api/connect/google`)
+                }
+              >
+                <div className="g-icon">
+                    <img src={gIcon.src}></img>
+                </div>
+                <span>Login with Google</span>
+              </button>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- client/pages/account/register.js
+++ client/pages/account/register.js
@@ -1,66 +1,58 @@
 import Link from "next/link";
 import SignUpForm from "../../components/auth/SignUpForm";
-import GoogleLogin from 'react-google-login';
+import gIcon from "../../public/img/svg/g-icon.svg";
 
 const Register = () => {
 
-    // const onSuccess = (res) => {
-    //     console.log("SUCCESS", res.profileObj);
-    // }
-
-    // const onFailure = (res) => {
-    //     console.log('FAILED', res);
-    // }
-
-    return <>
-        <div className="main-content">
-            <div className="main-body">
-                <div className="container register-page">
-                    <div className="row">
-                        <div className="col-lg-5 title">
-                            <span>Creeaza contul</span>
-                        </div>
-                        <div className="col-lg-5 subtitle">
-                            <span>Selecteaza sexul</span>
-                        </div>
-                        <div className="col-lg-5">
-                            <div className="form">
-                                <SignUpForm />
-                            </div>
-                        </div>
-                        <div className="col-lg-5 login">
-                            <span className="login-link">
-                                Have an account? <Link href='login'><a>Log In</a></Link>
-                            </span>
-                            <div className="split">
-                                <span className="line"></span>
-                                <span className="text">or</span>
-                                <span className="line"></span>
-                            </div>
-                            {/* <GoogleLogin 
-                                clientId={process.env.NEXT_PUBLIC_GA_ID}
-                                buttonText='Sign up with Google'
-                                onSuccess={onSuccess}
-                                onFailure={onFailure}
-                                cookiePolicy={'single_host_origin'}
-                                isSignedIn={true}
-                                className="g-auth"
-                            /> */}
-                            <div className='google-login'>
-                                <button onClick={() => 
-                                (window.location = `${process.env.NEXT_PUBLIC_STRAPI_ROOTURL}/api/connect/google`)}>Login via google</button>
-                            </div>
-                        </div>
-
-                        {/* simple footer */}
-                        {/* <div className="col-5 copyright">
-                            <span>&copy; Sickst</span>
-                        </div> */}
-                    </div>
+  return (
+    <>
+      <div className="main-content">
+        <div className="main-body">
+          <div className="container register-page">
+            <div className="row">
+              <div className="col-lg-5 title">
+                <span>Creeaza contul</span>
+              </div>
+              <div className="col-lg-5 subtitle">
+                <span>Selecteaza sexul</span>
+              </div>
+              <div className="col-lg-5">
+                <div className="form">
+                  <SignUpForm />
                 </div>
+              </div>
+              <div className="col-lg-5 login">
+                <span className="login-link">
+                  Have an account?{" "}
+                  <Link href="login">
+                    <a>Log In</a>
+                  </Link>
+                </span>
+                <div className="split">
+                  <span className="line"></span>
+                  <span className="text">or</span>
+                  <span className="line"></span>
+                </div>
+               
+                <button
+                  className="g-auth"
+                  onClick={() =>
+                    (window.location = `${process.env.NEXT_PUBLIC_STRAPI_ROOTURL}/api/connect/google`)
+                  }
+                >
+                  <div className="g-icon">
+                    <img src={gIcon.src}></img>
+                  </div>
+                  <span>Signup with Google</span>
+                </button>
+              </div>
+
             </div>
+          </div>
         </div>
+      </div>
     </>
-}
+  );
+};
 
 export default Register;
