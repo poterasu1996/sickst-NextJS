1b94da6efb6c0323ac569b876ca7cd5d98f945b5
Ionut <i.poterasu@yahoo.com>
2022-11-11 15:57:43 +0200

    [draft] subscriptions page

--- client/components/ShopPage/NewProductSection.js
+++ client/components/ShopPage/NewProductSection.js
@@ -1,114 +1,64 @@
+import { useEffect, useState } from "react";
 import { Star } from "react-feather";
 import Rating from "react-rating";
 import img from '../../public/img/creed-aventus.jpg';
+import Link from "next/link";
+import axios from '../../api/axios';
+
+const PRODUCTS_URL = "/products?populate=*";
 
 const NewProductSection = () => {
+    const [newProducts, setNewProducts] = useState();
+    useEffect(() => {
+        try {
+            axios.get(PRODUCTS_URL).then(resp => {
+                // get only first 6 new products in future
+                const newProdList = resp.data.data.filter(data => {
+                    return data.attributes.tags.data.find(tag => {
+                        if(tag.attributes.name === 'new') {
+                            return data;
+                        }
+                    })
+                })
+                setNewProducts([...newProdList]);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+
     return(<>
-        <div className="container new-product-section">
+        {newProducts && <div className="container new-product-section">
             <div className="new-product-section--header">
                 <div className="title">New fine fragrances</div>
                 {/* <div className="view-all">View all &gt;</div> */}
             </div>
             <div className="new-product-section--content">
-                <div className="new-product">
-                    <div className="new-product--img-wrapper">
-                        <img src={img.src}/>
-                    </div>
-                    <div className="new-product--details">
-                        <Rating
-                            fractions={2}
-                            initialRating={4.3}
-                            readonly={true}
-                            emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
-                            fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
-                        />
-                        <div className="name">Creed</div>
-                        <div className="model">Aventuous</div>
-                    </div>
-                </div>
-                <div className="new-product">
-                    <div className="new-product--img-wrapper">
-                        <img src={img.src}/>
-                    </div>
-                    <div className="new-product--details">
-                        <Rating
-                            fractions={2}
-                            initialRating={4.3}
-                            readonly={true}
-                            emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
-                            fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
-                        />
-                        <div className="name">Sue Phillips - Artisan Scent</div>
-                        <div className="model">eau de toilette</div>
-                    </div>
-                </div>
-                <div className="new-product">
-                    <div className="new-product--img-wrapper">
-                        <img src={img.src}/>
-                    </div>
-                    <div className="new-product--details">
-                        <Rating
-                            fractions={2}
-                            initialRating={4.3}
-                            readonly={true}
-                            emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
-                            fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
-                        />
-                        <div className="name">rag & bone</div>
-                        <div className="model">eau de toilette</div>
-                    </div>
-                </div>
-                <div className="new-product">
-                    <div className="new-product--img-wrapper">
-                        <img src={img.src}/>
-                    </div>
-                    <div className="new-product--details">
-                        <Rating
-                            fractions={2}
-                            initialRating={4.3}
-                            readonly={true}
-                            emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
-                            fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
-                        />
-                        <div className="name">rag & bone</div>
-                        <div className="model">eau de toilette</div>
-                    </div>
-                </div>
-                <div className="new-product">
-                    <div className="new-product--img-wrapper">
-                        <img src={img.src}/>
-                    </div>
-                    <div className="new-product--details">
-                        <Rating
-                            fractions={2}
-                            initialRating={4.3}
-                            readonly={true}
-                            emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
-                            fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
-                        />
-                        <div className="name">rag & bone</div>
-                        <div className="model">eau de toilette</div>
-                    </div>
-                </div>
-                <div className="new-product">
-                    <div className="new-product--img-wrapper">
-                        <img src={img.src}/>
-                    </div>
-                    <div className="new-product--details">
-                        <Rating
-                            fractions={2}
-                            initialRating={4.3}
-                            readonly={true}
-                            emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
-                            fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
-                        />
-                        <div className="name">rag & bone</div>
-                        <div className="model">eau de toilette</div>
-                    </div>
-                </div>
-
+                {newProducts && newProducts.map(product => (
+                    <Link href={`../product/${product.id}`}>
+                        <div className="new-product" key={product.id}>{console.log(product)}
+                            <div className="new-product--img-wrapper">
+                                <img src={
+                                    `${process.env.NEXT_PUBLIC_STRAPI_ROOTURL}` +
+                                    product.attributes.image.data[0].attributes.url
+                                }/>
+                            </div>
+                            <div className="new-product--details">
+                                <Rating
+                                    fractions={2}
+                                    initialRating={product.attributes.rating}
+                                    readonly={true}
+                                    emptySymbol={<Star size={15} fill="#babfc7" stroke="#babfc7" />}
+                                    fullSymbol={<Star size={15} fill="#cc3633" stroke="#cc3633" />}
+                                />
+                                <div className="name">{product.attributes.brand}</div>
+                                <div className="model">{product.attributes.model}</div>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
             </div>
-        </div>
+        </div>}
     </>);
 }
 
--- client/pages/subscriptions/index.js
+++ client/pages/subscriptions/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
 import cardImg1 from '../../public/img/mystery.jpg'
+import shield from '../../public/img/svg/shield.svg'
+import stripe1 from '../../public/img/svg/Stripe-Badge-Logo.svg'
+import stripe2 from '../../public/img/svg/Stripe-Outline-Logo.svg'
 
 function Subscriptions() {
   return (
-    <div className='subscription-page container'>
-        <div className='subscription-page--description'>
+    <div className='subscription-page'>
+        <div className='container subscription-page--description'>
             <div className='title'>Ce vei primi...</div>
             <div className='info'>
                 Un abonament catre Sickst. Se plateste lunar, se reinoieste automat, iar produsele se livreaza bilunar, la mijlocul si la sfarsitul fiecarei luni.
             </div>
         </div>
-        <div className='row black-cards'>
+        <div className='container black-cards'>
             <div className='col-4 card'>
                 <div className='card-image'>
                     <img src={cardImg1.src} />
@@ -62,6 +65,24 @@ function Subscriptions() {
                         <a className='button-second my-5'>Subscribe for 100 RON</a>
                     </Link>
                 </div>
+            </div>
+        </div>
+        <div className='container security-info'>
+            <div className='safe-checkout'>
+                <div className='title'>Guaranteed Safe Checkout</div>
+                <div className='d-inline-flex justify-content-center'>
+                    <img style={{width: '150px', height: '10rem'}} src={stripe1.src} />
+                    <img style={{margin: '0 1rem'}} src={shield.src} />
+                    <img style={{width: '150px', height: '10rem'}} src={stripe2.src} />
+                </div>
+            </div>
+            <div className='disclaimer'>
+                *By clicking “Pay”, you agree that your subscription will automatically renew every month until you cancel. 
+                After your initial one month charge of $8.47, your subscription will automatically renew every month at $16.95. 
+                The amount of each subsequent charge may change. Before any such charge is changed, you will be provided notice. 
+                If you want to cancel your subscription, you may do so by sending an e-mail to <b>office@sickst.com</b>. 
+                Before each renewal, we will send a reminder with the term and rate then in effect. If you do nothing,
+                we will charge the payment method you selected.
             </div>
         </div>
     </div>
