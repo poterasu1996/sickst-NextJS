95fbe0c0d483cff4816a1f47cd4fa631b4ba2705
AlexIlieFlorea <aflorea67@yahoo.com>
2023-03-19 12:02:18 +0200

    Te pup la valoare

--- client/pages/about-us/cards.tsx
+++ client/pages/about-us/cards.tsx
@@ -1,20 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Cards(props) {
   return (
-    <div className='card-team col-6'>
-        <img className='card-team--image' src={props.src}></img>
-        <h2 className='card-team--name'>
-        {props.name}
-        </h2>
-        <h4 className='card-team--position'>
-            {props.position}
-        </h4>
-        <p className='card-team--description'>
-            {props.description}
-        </p>
+    <div className="card-team col-12 col-md-6">
+      <img className="card-team--image" src={props.src}></img>
+      <h2 className="card-team--name">{props.name}</h2>
+      <h4 className="card-team--position">{props.position}</h4>
+      <p className="card-team--description">{props.description}</p>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
